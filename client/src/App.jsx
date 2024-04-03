3f14009913d8841800a079732ec54895afaed3ce
Martin Nebehay <martinnebe456@gmail.com>
2024-04-03 18:19:09 +0200

    Edit data for props (eventTable) and CSS created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,18 +5,54 @@ import EventTable from "./components/eventTable";
 
 function App() {
   const [events, setEvents] = useState([
-    { name: 'Jan Novák', department: 'IT', terminalId: '123', arrivalTime: '08:00', departureTime: '16:30' },
-    { name: 'Václav Hrbsky', department: 'FIN', terminalId: '456', arrivalTime: '09:00', departureTime: '17:30' },
-    { name: 'Petr Novák', department: 'IT', terminalId: '789', arrivalTime: '10:00', departureTime: '18:30' },
-    { name: 'Pavel Novák', department: 'HR', terminalId: '321', arrivalTime: '11:00', departureTime: '19:30' },
-    { name: 'Jiri Novák', department: 'IT', terminalId: '654', arrivalTime: '12:00', departureTime: '20:30' },
-    { name: 'Václav Novák', department: 'IT', terminalId: '987', arrivalTime: '13:00', departureTime: '21:30' },
-    { name: 'Jan Hrbsky', department: 'IT', terminalId: '147', arrivalTime: '14:00', departureTime: '22:30' },
-    { name: 'Pavel Hrbsky', department: 'IT', terminalId: '258', arrivalTime: '15:00', departureTime: '23:30' },
-    { name: 'Jiri Hrbsky', department: 'HR', terminalId: '369', arrivalTime: '16:00', departureTime: '00:30' },
-    { name: 'Václav Hrbsky', department: 'FIN', terminalId: '159', arrivalTime: '17:00', departureTime: '01:30' },
-    { name: 'Pavel Hrbsky', department: 'IT', terminalId: '753', arrivalTime: '18:00', departureTime: '02:30' },
-    { name: 'Jiri Hrbsky', department: 'IT', terminalId: '147', arrivalTime: '19:00', departureTime: '03:30' },
+    {
+      name: "Carl Berry",
+      department: "IT",
+      terminalId: "123",
+      date: "2022-01-01",
+      arrivalTime: "08:00",
+      departureTime: "16:30",
+    },
+    {
+      name: "John Doe",
+      department: "HR",
+      terminalId: "456",
+      date: "2022-01-01",
+      arrivalTime: "08:00",
+      departureTime: "16:30",
+    },
+    {
+      name: "Paul Smith",
+      department: "IT",
+      terminalId: "789",
+      date: "2022-01-01",
+      arrivalTime: "08:00",
+      departureTime: "16:30",
+    },
+    {
+      name: "Carl Berry",
+      department: "IT",
+      terminalId: "123",
+      date: "2022-01-02",
+      arrivalTime: "08:00",
+      departureTime: "14:40",
+    },
+    {
+      name: "John Doe",
+      department: "HR",
+      terminalId: "456",
+      date: "2022-01-02",
+      arrivalTime: "08:00",
+      departureTime: "15:31",
+    },
+    {
+      name: "Paul Smith",
+      department: "IT",
+      terminalId: "789",
+      date: "2022-01-02",
+      arrivalTime: "08:00",
+      departureTime: "16:30",
+    },
   ]);
 
   return (
--- client/src/components/eventTable.jsx
+++ client/src/components/eventTable.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const EventTable = ({ events }) => {
   return (
     <table>
       <thead>
         <tr>
-          <th>Jméno</th>
-          <th>Oddělení</th>
-          <th>ID Terminálu</th>
-          <th>Čas Příchodu</th>
-          <th>Čas Odchodu</th>
+          <th>Name</th>
+          <th>Department</th>
+          <th>Terminal ID</th>
+          <th>Date</th>
+          <th>Arrival Time</th>
+          <th>Departure Time</th>
         </tr>
       </thead>
       <tbody>
@@ -18,6 +19,7 @@ const EventTable = ({ events }) => {
             <td>{event.name}</td>
             <td>{event.department}</td>
             <td>{event.terminalId}</td>
+            <td>{event.date}</td>
             <td>{event.arrivalTime}</td>
             <td>{event.departureTime}</td>
           </tr>
