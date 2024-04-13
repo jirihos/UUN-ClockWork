720891e8df7f0dd81e159857be80cfc17952085f
jirihos <7355480+jirihos@users.noreply.github.com>
2024-04-13 20:54:31 +0200

    Add Mongo object ID validation

--- server/schema/user-schema.js
+++ server/schema/user-schema.js
@@ -1,4 +1,10 @@
 const Joi = require("joi");
+Joi.objectId = require("joi-objectid")(Joi);
+
+// Example validation of Mongo object ID
+// Joi.object({
+//   id: Joi.objectId(),
+// });
 
 exports.userLoginSchema = Joi.object({
   username: Joi.string().alphanum().min(3).max(20).required(),
