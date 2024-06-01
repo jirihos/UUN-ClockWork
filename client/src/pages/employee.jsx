d8d9f4e09fdde640681379733ef0e76f2245d816
Martin Nebehay <martinnebe456@gmail.com>
2024-06-01 10:21:57 +0200

    Fix issue with token

--- client/src/pages/employee.jsx
+++ client/src/pages/employee.jsx
@@ -1,13 +1,12 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useEffect, useState, useParams } from "react";
 import axios from "axios";
 import Header from "../components/header";
 
-const EmployeeDetails = () => {
-  const { code } = useParams();
+const Employee = () => {
   const [shiftData, setShiftData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const { code } = useParams();
 
   useEffect(() => {
     const fetchShiftData = async () => {
@@ -15,7 +14,7 @@ const EmployeeDetails = () => {
         const response = await axios.get(
           `http://localhost:3000/api/event/listShiftsByEmployeeCode?pageIndex=0&pageSize=30&employeeCode=${code}`,
         );
-        setShiftData(response.data);
+        setShiftData(response.data.items);
         setLoading(false);
       } catch (err) {
         setError(err);
@@ -26,35 +25,34 @@ const EmployeeDetails = () => {
     fetchShiftData();
   }, [code]);
 
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error loading data: {error.message}</div>;
+
   return (
     <div>
       <Header />
       <h1>Employee Code: {code}</h1>
-      {loading && <div>Loading...</div>}
-      {error && <div>Error loading data</div>}
-      {!loading && !error && (
-        <table>
-          <thead>
-            <tr>
-              <th>Date</th>
-              <th>Arrival Time</th>
-              <th>Departure Time</th>
-            </tr>
-          </thead>
-          <tbody>
-            {shiftData &&
-              shiftData.map((entry, index) => (
-                <tr key={index}>
-                  <td>{entry.date}</td>
-                  <td>{entry.arrival}</td>
-                  <td>{entry.departure}</td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
-      )}
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Arrival Time</th>
+            <th>Departure Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          {shiftData &&
+            shiftData.map((entry, index) => (
+              <tr key={index}>
+                <td>{new Date(entry.arrivalTimestamp).toLocaleDateString()}</td>
+                <td>{new Date(entry.arrivalTimestamp).toLocaleTimeString()}</td>
+                <td>{new Date(entry.leaveTimestamp).toLocaleTimeString()}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </div>
   );
 };
 
-export default EmployeeDetails;
+export default Employee;
