0bedfa0e27dfc68ead8faa7c9006c6597dcfa5b4
Mvrty00 <97028420+MaresMartin00@users.noreply.github.com>
2024-06-01 18:43:35 +0200

    Department notif

--- client/src/components/DepartmentList.jsx
+++ client/src/components/DepartmentList.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { Table, Button, Icon } from "semantic-ui-react";
+import { Table, Button, Icon, Modal, Message } from "semantic-ui-react";
 import { useCall } from "../helpers/call-helper";
 import ModalAddDepartment from "./ModalAddDepartment";
 import Error from "./Error";
@@ -10,6 +10,10 @@ const DepartmentList = () => {
   const [error, setError] = useState(null);
   const [departments, setDepartments] = useState([]);
   const [openDepartmentModal, setOpenDepartmentModal] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [currentDepartmentId, setCurrentDepartmentId] = useState(null);
+  const [deleteError, setDeleteError] = useState(null);
+  const [deleteSuccess, setDeleteSuccess] = useState(null);
 
   useEffect(() => {
     const loadData = async () => {
@@ -25,24 +29,74 @@ const DepartmentList = () => {
     loadData();
   }, [call]);
 
+  useEffect(() => {
+    if (deleteError) {
+      const timer = setTimeout(() => {
+        setDeleteError(null);
+      }, 5000);
+      return () => clearTimeout(timer);
+    }
+  }, [deleteError]);
+
+  useEffect(() => {
+    if (deleteSuccess) {
+      const timer = setTimeout(() => {
+        setDeleteSuccess(null);
+      }, 5000);
+      return () => clearTimeout(timer);
+    }
+  }, [deleteSuccess]);
+
   const handleAddDepartmentSuccess = (newDepartment) => {
     setDepartments([...departments, newDepartment]);
   };
 
-  const handleDeleteDepartment = async (departmentId) => {
+  const handleDeleteDepartment = async () => {
     try {
-      await call("POST", "/api/department/delete", { _id: departmentId });
+      await call("POST", "/api/department/delete", {
+        _id: currentDepartmentId,
+      });
       const data = await call("GET", "/api/department/list");
       setDepartments(data.items);
+      setModalOpen(false);
+      setDeleteSuccess("Department was successfully deleted.");
+      setDeleteError(null);
     } catch (error) {
+      if (error) {
+        setDeleteError(
+          "Department can not be deleted because there is employee.",
+        );
+      }
       console.error(error);
+      setModalOpen(false);
     }
+  };
+
+  const openModal = (departmentId) => {
+    setCurrentDepartmentId(departmentId);
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
   };
 
   return (
     <>
       {!error ? (
         <>
+          {deleteError && (
+            <Message negative>
+              <Message.Header>Error</Message.Header>
+              <p>{deleteError}</p>
+            </Message>
+          )}
+          {deleteSuccess && (
+            <Message positive>
+              <Message.Header>Success</Message.Header>
+              <p>{deleteSuccess}</p>
+            </Message>
+          )}
           <Table>
             <Table.Header>
               <Table.Row>
@@ -66,7 +120,7 @@ const DepartmentList = () => {
                       color="teal"
                       icon
                       labelPosition="left"
-                      onClick={() => handleDeleteDepartment(department._id)}
+                      onClick={() => openModal(department._id)}
                     >
                       <Icon name="trash" />
                       Delete
@@ -81,6 +135,21 @@ const DepartmentList = () => {
             onClose={() => setOpenDepartmentModal(false)}
             onSuccess={handleAddDepartmentSuccess}
           />
+
+          <Modal open={modalOpen} onClose={closeModal} size="small">
+            <Modal.Header>Confirm Deletion</Modal.Header>
+            <Modal.Content>
+              <p>Are you sure you want to delete this department?</p>
+            </Modal.Content>
+            <Modal.Actions>
+              <Button onClick={closeModal} negative>
+                No
+              </Button>
+              <Button onClick={handleDeleteDepartment} positive>
+                Yes
+              </Button>
+            </Modal.Actions>
+          </Modal>
         </>
       ) : (
         <Error error={error} />
